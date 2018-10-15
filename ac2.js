291b378fc2764c82fb8410d6b6fed7a8a68ced6e
andras29 <44159972+andras29@users.noreply.github.com>
2018-10-15 13:25:54 +0300

    Update ac2.js

--- ac2.js
+++ ac2.js
@@ -1,1 +1,24 @@
 document.getElementById("id_business_version").innerHTML="Paragraph changed";
+
+function rezolva()
+{
+  var a =document.getElementById("id_a").value;
+  var b =document.getElementById("id_b").value;
+  var c=document.getElementById("id_c").value;
+  //rezolvare
+  
+  var delta= b*b-4*a*c;
+  var x1_re,x1_im,x2_re,x2_im;
+  if(delta >=0){
+       x1_re=(-b+Math.sqrt(delta))/(2*a);
+      x2_re=(-b-Math.sqrt(delta))/(2*a);
+    x1_im=x2_im=0;
+  }
+  else{
+    x1_re=-b/(2*a);
+    x1_im=Math.sqrt(-delta)/(2*a);
+    x2_re=-b/(2*a);
+    x2_im=-Math.sqrt(-delta)/(2*a);
+  }
+  
+  //scriere
