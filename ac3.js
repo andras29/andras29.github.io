7ec9de3e36627de321db47c378017d8f9f201dd6
andras29 <44159972+andras29@users.noreply.github.com>
2018-10-15 13:48:25 +0300

    Update ac3.js

--- ac3.js
+++ ac3.js
@@ -10,6 +10,19 @@ function citire(){
 
 function rezolvare()
 {
+    var delta= coeficienti.b*coeficienti.b-4*coeficienti.a*coeficienti.c;
+  var x1_re,x1_im,x2_re,x2_im;
+  if(delta >=0){
+       x1_re=(-coeficienti.b+Math.sqrt(delta))/(2*coeficienti.a);
+      x2_re=(-coeficienti.b-Math.sqrt(delta))/(2*a);
+    x1_im=x2_im=0;
+  }
+  else{
+    x1_re=-coeficienti.b/(2*coeficienti.a);
+    x1_im=Math.sqrt(-delta)/(2*coeficienti.a);
+    x2_re=-coeficienti.b/(2*coeficienti.a);
+    x2_im=-Math.sqrt(-delta)/(2*coeficienti.a);
+  }
    return
 }
 
