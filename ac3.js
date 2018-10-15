2c0881ff8a8261b8b00a4c98d34da07a4e548264
andras29 <44159972+andras29@users.noreply.github.com>
2018-10-15 13:43:51 +0300

    Update ac3.js

--- ac3.js
+++ ac3.js
@@ -1,6 +1,11 @@
 
 function citire(){
-  return;
+   var a =document.getElementById("id_a").value;
+  var b =document.getElementById("id_b").value;
+  var c=document.getElementById("id_c").value;
+  
+  var coeficienti={a:_a,b:_b,c:_c};
+  return coeficienti;
 }
 
 function rezolvare()
